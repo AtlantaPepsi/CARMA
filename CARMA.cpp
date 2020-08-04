8d6f7a24e7d8648834dfb592472f3b5b1f519bfe
AtlantaPepsi <hyj1999110@gmail.com>
2020-08-03 22:05:36 -0400

    fixing odd number dimension split

--- CARMA.cpp
+++ CARMA.cpp
@@ -62,6 +62,7 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				int new_param[3] = {0, 0, 0};
 				MPI_Request req; //dummy
 				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req);
+				MPI_Wait(&req,MPI_STATUS_IGNORE);
 				MPI_Request_free(&req);
 				continue;
 			}
@@ -71,15 +72,20 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				colors[i] = 1;
 				int new_param[3] = {m, k, n};
 				//printf("spliting: source:%d target:%d m:%d n:%d k:%d\n", rank, temp, m, n, k);
-				MPI_Request req; //dummy
-				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req);
+				MPI_Request req1, req2, req3; //dummy
+				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req1);
 
 				//split A horizantally
 				double* A_bot = *A + (m * k);
 
-				MPI_Isend(A_bot, m*k, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Isend(*B, k*n, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Request_free(&req);
+				MPI_Isend(A_bot, m*k, MPI_DOUBLE, temp, 0, comm, &req2);
+				MPI_Isend(*B, k*n, MPI_DOUBLE, temp, 0, comm, &req3);
+				MPI_Wait(&req1,MPI_STATUS_IGNORE);
+				MPI_Wait(&req2,MPI_STATUS_IGNORE);
+				MPI_Wait(&req3,MPI_STATUS_IGNORE);
+				MPI_Request_free(&req1);
+				MPI_Request_free(&req2);
+				MPI_Request_free(&req3);
 				continue;
 			}
 
@@ -88,8 +94,8 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				colors[i] = 3;
 				int new_param[3] = {m, k, n};
 				//printf("spliting: source:%d target:%d m:%d n:%d k:%d\n", rank, temp, m, n, k);
-				MPI_Request req; //dummy
-				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req);
+				MPI_Request req1, req2, req3; //dummy
+				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req1);
 
 				//split B vertically
 				double* B_left = (double*) malloc(sizeof(double)*(k*n));
@@ -100,9 +106,14 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 					copy(*B + j*2*n + n, *B + (j+1)*2*n, B_right + j*n);
 				}
 				//printf("copy ends: %d\n", rank);
-				MPI_Isend(*A, m*k, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Isend(B_right, k*n, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Request_free(&req);
+				MPI_Isend(*A, m*k, MPI_DOUBLE, temp, 0, comm, &req2);
+				MPI_Isend(B_right, k*n, MPI_DOUBLE, temp, 0, comm, &req3);
+				MPI_Wait(&req1,MPI_STATUS_IGNORE);
+				MPI_Wait(&req2,MPI_STATUS_IGNORE);
+				MPI_Wait(&req3,MPI_STATUS_IGNORE);
+				MPI_Request_free(&req1);
+				MPI_Request_free(&req2);
+				MPI_Request_free(&req3);
 
 				free(B_right);
 				free(*B);
@@ -115,8 +126,8 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				colors[i] = 2;
 				int new_param[3] = {m, k, n};
 				//printf("spliting: source:%d target:%d m:%d n:%d k:%d\n", rank, temp, m, n, k);
-				MPI_Request req; //dummy
-				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req);
+				MPI_Request req1, req2, req3; //dummy
+				MPI_Isend(new_param, 3, MPI_INT, temp, 0, comm, &req1);
 
 				//split A vertically
 				double* A_left = (double*) malloc(sizeof(double)*(m*k));
@@ -130,9 +141,14 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				//split B horizantally
 				double* B_bot = *B + (k * n);
 
-				MPI_Isend(A_right, m*k, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Isend(B_bot, k*n, MPI_DOUBLE, temp, 0, comm, &req);
-				MPI_Request_free(&req);
+				MPI_Isend(A_right, m*k, MPI_DOUBLE, temp, 0, comm, &req2);
+				MPI_Isend(B_bot, k*n, MPI_DOUBLE, temp, 0, comm, &req3);
+				MPI_Wait(&req1,MPI_STATUS_IGNORE);
+				MPI_Wait(&req2,MPI_STATUS_IGNORE);
+				MPI_Wait(&req3,MPI_STATUS_IGNORE);
+				MPI_Request_free(&req1);
+				MPI_Request_free(&req2);
+				MPI_Request_free(&req3);
 
 				free(A_right);
 				free(*A);
@@ -155,11 +171,11 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 */
 	//printf("rank %d finishes\n", rank);
 	for (int i = level - 1; i >= log; i--) {
-		//printf("rank %d receiving from %d\n",rank, temp);
 		temp = rank + (1<<i);
 
 		if (temp < size) {
 
+		printf("rank %d receiving from %d\n",rank, temp);
 			if (colors[i] == 4) {
 				continue;
 			}
@@ -167,6 +183,7 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 			if (colors[i] == 3) {
 				double* C_right = (double*) malloc(sizeof(double)*(m*n));
 				double* new_C = (double*) malloc(sizeof(double)*(m*2*n));
+				if (temp == 10) printf("!!!!%d: %d,%d\n",rank,m,n);
 				MPI_Recv(C_right, m*n, MPI_DOUBLE, temp, 0, comm, MPI_STATUS_IGNORE);
 				for(int j = 0; j < m; j++) {
 					copy(*C + n*j, *C + (j+1)*n, new_C + j*(2*n));
@@ -176,12 +193,15 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				free(*C);
 				*C = new_C;
 				n *= 2;
+		printf("rank %d roger %d: %d\n",rank, temp, colors[i]);
 				continue;
 			}
 
 			if (colors[i] == 1) {
 				double* C_bot = (double*) malloc(sizeof(double)*(m*n));
 				double* new_C = (double*) malloc(sizeof(double)*(m*2*n));
+				if (temp == 10) printf("!!!!%d: %d,%d\n",rank,m,n);
+		
 				MPI_Recv(C_bot, m*n, MPI_DOUBLE, temp, 0, comm, MPI_STATUS_IGNORE);
 				copy(*C, *C + m*n, new_C);
 				copy(C_bot, C_bot + m*n, new_C + m*n);
@@ -189,12 +209,14 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				free(*C);
 				*C = new_C;
 				m *= 2;
+		printf("rank %d roger %d: %d\n",rank, temp, colors[i]);
 				continue;
 			}
 
 			if (colors[i] == 2) {
 
 				double* new_C = (double*) malloc(sizeof(double)*(m*n));
+				if (temp == 10) printf("!!!!%d: %d,%d\n",rank,m,n);
 
 				//MPI_Reduce(*C, new_C, m*n, MPI_DOUBLE, MPI_SUM, rank, comm01);
 				MPI_Recv(new_C, m*n, MPI_DOUBLE, temp, 0, comm, MPI_STATUS_IGNORE);
@@ -207,6 +229,7 @@ void CARMA(double** A, double** B, double** C, int* param, MPI_Comm comm)  //pas
 				free(new_C);
 
 				k *= 2;
+		printf("rank %d roger %d: %d\n",rank, temp, colors[i]);
 				continue;
 
 			}
---
+++ evaluations/CARMA_test.cpp
@@ -0,0 +1,109 @@
+//#include <gtest/gtest.h>
+#include <math.h>
+#include <random>
+#include <vector>
+#include <iostream>
+#include "mkl.h"
+#include <chrono>
+
+void test(int m, int k, int n) {
+
+
+	int rank, p;
+	double *A;
+	double *B;
+	double *C;
+	int *param=(int*)malloc(sizeof(int)*3);
+	double* expected_C = new double[m*n];
+
+		/*
+		double temp[4*4] = {10., -1., 2., 0.,
+			-1., 11., -1., 3.,
+			2., -1., 10., -1.,
+			0.0, 3., -1., 8.};
+		double temp2[4*4] = {27., 4., -8., 3.,
+			-1., -6., -12., 3.,
+			-9., 13., 37., 0.,
+			6., -3., -1., 20.};
+		A = (double*)malloc(sizeof(double)*16);
+		B = (double*)malloc(sizeof(double)*16);
+		C = (double*)malloc(sizeof(double)*16);
+		std::copy(temp, temp+16, A);
+		std::copy(temp2, temp2+16, B);
+		*/
+		std::vector<double> a(m*k);
+		std::vector<double> b(k*n);
+		A = (double*)malloc(sizeof(double)*m*k);
+		B = (double*)malloc(sizeof(double)*k*n);
+		C = (double*)malloc(sizeof(double)*m*n);
+		
+
+		std::normal_distribution<double> distribution(200.0, 20.0);
+
+		std::default_random_engine generator;
+		for (int i = 0;i < a.size();i++) {
+			a[i] = distribution(generator);
+		}
+		for (int i = 0;i < b.size();i++) {
+			b[i] = distribution(generator);
+		}
+
+		std::copy(a.begin(), a.end(), A);
+		std::copy(b.begin(), b.end(), B);
+		
+		auto t1 = std::chrono::high_resolution_clock::now();
+		cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+					m, n, k, 1, A, k, B, n, 0, expected_C, n);
+		auto t2 = std::chrono::high_resolution_clock::now();
+
+    		std::chrono::duration<double, std::milli> duration = t2 - t1;
+
+
+
+		printf("A:%p B:%p C:%p\n",&A,&B,&C);
+	   //printf("%f\n",C[0]);
+		param[0] = m;
+		param[1] = k;
+		param[2] = n;
+
+
+//	printf("%p\n",&A);
+
+	//CARMA(&A, &B, &C, param, MPI_COMM_WORLD);
+
+    	
+		/*for (int i = 0; i < m*n; i++) {
+			if(abs(expected_C[i]-C[i]) > 1e-3) {
+				printf(" element y[%d] is wrong: %f, %f\n", i, expected_C[i], C[i]);
+			}
+		}*/
+		printf("time: %f ms\n", duration.count());
+		free(A);
+		free(B);
+		free(C);
+	delete[] expected_C;
+	free(param);
+
+}
+
+int main(int argc, char* argv[]) {
+	int result = 0;
+
+	//testing::InitGoogleTest(&argc, argv);
+	int m = atoi(argv[1]);
+	int k = atoi(argv[2]);
+	int n = atoi(argv[3]);
+
+	test(m,k,n);
+	int rank;
+
+		printf("%d done\n",rank);
+
+
+
+	//printf("congrats!\n");
+	return 0;
+
+
+
+}
